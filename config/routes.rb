a22d83fb5e617305ae02523caf9163a54fcf802a
Seki Ryota <sekiryouta749@gmail.com>
2023-11-13 23:59:59 +0900

    投稿機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: 'homes#top'
+  post 'posts' => 'posts#create'
   
 end
